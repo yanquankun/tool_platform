6904ff60dfed24c3c90ff658408c8372be6a9378
yanquankun <17600610907@163.com>
2023-08-29 13:56:02 +0800

    feat: 增加home

---
+++ static/home/home.entry.ts
@@ -0,0 +1,15 @@
+const a = 1;
+type test = {
+  a: number;
+  b: number;
+};
+interface ITest2 {
+  a: string;
+}
+
+const fn = (p1: number, p2: string): string => {
+  return '123';
+};
+
+const params: Parameters<typeof fn>[0] = 123;
+const returnTest: ReturnType<typeof fn> = '123';
--- static/index.tsx
+++
@@ -1,8 +0,0 @@
-const a = 1;
-type test = {
-  a: number;
-  b: number;
-};
-interface ITest2 {
-  a: string;
-}

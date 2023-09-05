b55f248db9109d0a36997dbf7a640d99e4ca4e44
yanquankun <17600610907@163.com>
2023-09-05 10:01:22 +0800

    chore: webpack构建优化

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,8 @@ module.exports = Object.keys(entryPathMap).map((entryDirectoryName, index) => {
     devtool: isProduction === 'production' ? 'nosources-source-map ' : 'eval-cheap-module-source-map',
 
     resolve: {
-      extensions: ['.ts', '.tsx', '.js', '.json'],
+      // 尝试按顺序解析这些后缀名。如果有多个文件有相同的名字，但后缀名不同，webpack 会解析列在数组首位的后缀的文件 并跳过其余的后缀
+      extensions: ['.ts', '.tsx', '.jsx', '.js', '.json'],
     },
 
     module: {

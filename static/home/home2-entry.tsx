3e03af73cf5975fc105fe1c0903ca975d8c426e6
Mint <17600610907@163.com>
2023-09-15 01:35:09 +0800

    feat: my-blog init

--- static/home/home2-entry.tsx
+++ static/home/home2-entry.tsx
@@ -2,4 +2,5 @@ import { createRoot } from 'react-dom/client';
 import { App } from './pages/home2';
 
 const root = createRoot(document.getElementById('app') as HTMLElement);
+
 root.render(<App />);

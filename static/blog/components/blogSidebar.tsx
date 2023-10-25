0cbbcd94afb979e8be9cc070e6d9a4bcbc0f09d5
yanquankun <17600610907@163.com>
2023-10-25 14:38:20 +0800

    feat: blog暂存

--- static/blog/components/blogSidebar.tsx
+++ static/blog/components/blogSidebar.tsx
@@ -1,34 +1,63 @@
-import { FC, useState } from 'react';
-import { Steps, Row, Col } from 'antd';
+import { FC, Fragment, useEffect, useState } from 'react';
+import { Steps, Row, Col, Space } from 'antd';
+import { css } from '@emotion/css';
+import { StepBackwardOutlined } from '@ant-design/icons';
+interface IProps {
+  blogChange: (blogId: number) => void;
+}
 
-export const BlogSidebar: FC = () => {
+export const BlogSidebar: FC<IProps> = (props): JSX.Element => {
   const [current, setCurrent] = useState(0);
 
-  const onChange = (value: number) => {
-    console.log('onChange:', current);
-    setCurrent(value);
-  };
   const description = 'This is a description.';
+  const blogList = [
+    {
+      blogId: 1,
+      title: 'Step 1',
+      description,
+      icon: <Fragment></Fragment>,
+    },
+    {
+      blogId: 2,
+      title: 'Step 2',
+      description,
+      icon: <StepBackwardOutlined />,
+    },
+  ];
 
-  return (
-    <Steps
-      current={current}
-      onChange={onChange}
+  useEffect(() => {
+    blogList.length && props.blogChange(blogList[0].blogId);
+  }, []);
+
+  const onChange = (blogId: number) => {
+    console.log(blogId);
+    props.blogChange(blogId);
+  };
+
+  return blogList.length ? (
+    <Space
       direction="vertical"
-      items={[
-        {
-          title: 'Step 1',
-          description,
-        },
-        {
-          title: 'Step 2',
-          description,
-        },
-        {
-          title: 'Step 3',
-          description,
-        },
-      ]}
-    />
+      css={{
+        border: '1px solid',
+        minHeight: '120px',
+      }}
+    >
+      {blogList.map((item: any) => {
+        return (
+          <Col key={item.blogId} onClick={() => onChange(item.blogId)}>
+            <span>{item.title}</span>
+            <span
+              css={{
+                marginLeft: '10px',
+              }}
+            >
+              {item.icon}
+            </span>
+          </Col>
+        );
+      })}
+    </Space>
+  ) : (
+    <Fragment></Fragment>
   );
 };
--- static/blog/pages/blog.tsx
+++ static/blog/pages/blog.tsx
@@ -4,11 +4,15 @@ import { BlogSidebar } from '../components/blogSidebar';
 import { WaterMark } from '@ant-design/pro-components';
 
 export const App: FC = function () {
+  const blogChange = (blogId: number) => {
+    console.log('blogId is', blogId);
+  };
+
   return (
     <WaterMark content={['闫全堃', '17600610907@164.com']}>
       <Row gutter={16}>
         <Col span={4}>
-          <BlogSidebar />
+          <BlogSidebar blogChange={blogChange} />
         </Col>
         <Col span={19} offset={1}>
           123

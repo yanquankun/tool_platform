59aef294c7c586d89e55b014ba0935b6c17526f1
yanquankun <17600610907@163.com>
2024-03-11 17:01:52 +0800

    feat: blog列表顺序调整

--- static/blog/pages/blog.tsx
+++ static/blog/pages/blog.tsx
@@ -79,8 +79,8 @@ export const App: FC = function () {
           content:
             '从这里开始第一篇吧，以前习惯把个人心得、学习笔记记录到印象笔记中，没有想过自己整一个个人博客，这次趁着空闲，就自己实现一个吧，由于时间问题，这次用的是静态数据，未来将使用nodejs搭建后端服务。23年经历了很多事，尤其是最近，遇到了一件糟心事，心态也发生了一些变化，也成熟起来了，现在的想法除了希望家人健康平安，就是搞钱了，努力赚钱！后续会持续更新本网站，加入更多元素。',
         },
-        ...blogList,
         ...publishBlogList,
+        ...blogList,
       ],
       total_count: total_count + (publish_total_count || 0) + 1,
     });

525e6c371ab7f77562889ffa6f33ba419469fe55
yanquankun <yanquankun@meituan.com>
2024-04-26 18:27:12 +0800

    feat: 内容区域暂存

--- static/blog/components/blogV2/content.tsx
+++ static/blog/components/blogV2/content.tsx
@@ -1,7 +1,19 @@
-import React, { FC } from 'react';
-import { render } from 'react-dom';
+import { FC } from 'react';
 import { css } from '@emotion/css';
+import { Watermark } from 'antd';
+
+const commonStyle = {
+  content: css`
+    padding: 5rem 5rem 3rem 23rem;
+    overflow-y: auto;
+    min-height: calc(100vh - 10rem);
+  `,
+};
 
 export const Content: FC = function (): JSX.Element {
-  return <></>;
+  return (
+    <Watermark content="堃堃Blog">
+      <div className={commonStyle.content}>123</div>
+    </Watermark>
+  );
 };
--- static/blog/pages/blogV2.tsx
+++ static/blog/pages/blogV2.tsx
@@ -1,12 +1,14 @@
 import { FC, useState, useEffect, useMemo, useCallback, Fragment } from 'react';
 import { Header } from '../components/blogV2/header';
 import { Slider } from '../components/blogV2/slider';
+import { Content } from '../components/blogV2/content';
 
 export const App: FC = () => {
   return (
     <Fragment>
       <Header />
       <Slider />
+      <Content />
     </Fragment>
   );
 };

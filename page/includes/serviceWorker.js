884d64e876a38bf4a280c62a731b418dd1e2fa29
yanquankun <yanquankun@58.com>
2025-03-18 13:47:00 +0800

    feat: sw comment

--- page/includes/serviceWorker.js
+++ page/includes/serviceWorker.js
@@ -3,15 +3,11 @@ const CACHE_NAME = 'blog-cache-v1';
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([
-        '/', // 首页
-        '/offline/offline', // 备用的离线页面
-        '/cdn/gongzhonghao-qrcode.jpg',
-        '/cdn/mini-program-qrcode.png',
-      ]);
+      return cache.addAll(['/', '/offline/offline', '/cdn/gongzhonghao-qrcode.jpg', '/cdn/mini-program-qrcode.png']);
     })
   );
-  self.skipWaiting(); // 立即激活新的 Service Worker
+  /** 立即激活新的 Service Worker */
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', (event) => {
@@ -26,12 +22,10 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     fetch(event.request)
       .then((response) => {
-        // 如果请求成功，检查响应是否有效
         if (!response || response.status !== 200 || response.type !== 'basic') {
           return response;
         }
 
-        // 克隆响应并存入缓存
         const responseToCache = response.clone();
         caches.open(CACHE_NAME).then((cache) => {
           cache.put(event.request, responseToCache);
@@ -40,29 +34,27 @@ self.addEventListener('fetch', (event) => {
         return response;
       })
       .catch(() => {
-        // **只有在 navigator.onLine 为 false 时才返回离线页面**
+        /** 只有在 navigator.onLine 为 false 时才返回离线页面 */
         if (!navigator.onLine && event.request.mode === 'navigate') {
           return caches.match('/offline/offline');
         }
 
-        // 如果不是离线状态，尝试从缓存中获取资源
         return caches.match(event.request);
       })
   );
 });
 
-// 监听 activate 事件，清理旧缓存
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
           if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName); // 删除旧缓存
+            return caches.delete(cacheName);
           }
         })
       );
     })
   );
-  self.clients.claim(); // 立即控制所有页面
+  self.clients.claim();
 });
